9bf25bad65dbb07ca1d8c9d0588f3a713a712c1e
MicheleMarrandino <1michelemarrandino1@gmail.com>
2022-04-22 14:22:36 +0200

    Added requested functionality. ⚡

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  username: string;
+  psw: string;
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(): void{
+    if(!this.username || !this.psw){
+    alert(`Inserisci entrambi i campi sottostanti.`);
+    return;
+    }
+    alert(`Ciao ${this.username}!\nLa tua password è: ${this.psw}`);
   }
 
 }
